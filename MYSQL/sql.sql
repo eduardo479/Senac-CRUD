a0719d842329c5440cb2ba509183745801f45153
eduardo479 <duduapelao958@gmail.com>
2025-02-17 16:33:57 -0300

    Add files via upload

--- MYSQL/sql.sql
+++ MYSQL/sql.sql
@@ -1,8 +1,16 @@
 USE Empresa;
 
-CREATE TABLE funcionarios (
+CREATE TABLE funcionarios(
     id int AUTO_INCREMENT PRIMARY KEY,
-    funcionario VARCHAR (60) NOT NULL
-    cidade VARCHAR(50) NOT NULL,
-    data_contratacao DATE NOT NULL
+    cliente VARCHAR (60) NOT NULL
+    endereco VARCHAR(50) NOT NULL,
+    telefone VARCHAR DATE NOT NULL,
+    cep VARCHAR (15) NOT NULL
+)
+
+CREATE TABLE produtos(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    produto VARCHAR (60) NOT NULL
+    estoquue VARCHAR(50) NOT NULL,
+    preco VARCHAR (50) DATE NOT NULL
 )
